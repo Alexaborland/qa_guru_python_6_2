612ae36860e4d62dd3af48e4ff2faad2b0066fb3
Alexaborland <Borland3711@gmail.com>
2023-06-17 14:18:14 +0300

    Добавила фикстуры

---
+++ Test_homework/test_homework.py
@@ -0,0 +1,19 @@
+import pytest
+from selene import browser
+from selene import be, have
+
+@pytest.fixture()
+def browser_open():
+    browser.config.window_width = 1600
+    browser.config.window_height = 800
+    browser.open('https://google.com')
+
+
+def test_search_success(browser_open):
+    browser.element('[name="q"]').should(be.blank).type('yashaka/selene').press_enter()
+    browser.element('[id="search"]').should(have.text('Selene - User-oriented Web UI browser tests in Python'))
+
+
+def test_search_nothing(browser_open):
+    browser.element('[name="q"]').should(be.blank).type('kdkdkfkkfkfkjjkkkkkuytyf').press_enter()
+    browser.element('[id="topstuff"]').should(have.text('По запросу kdkdkfkkfkfkjjkkkkkuytyf ничего не найдено.'))
